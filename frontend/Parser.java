d476a9c7cf78c32e625b644366814815dc53c329
BUAA-bond <2401829463@qq.com>
2024-10-10 22:23:39 +0800

    词法分析输出小修改

--- frontend/Parser.java
+++ frontend/Parser.java
@@ -12,42 +12,6 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Parser {
-    public static HashMap<NodeType, String> nodeType = new HashMap<>() {{
-        put(NodeType.AddExp, "<AddExp>\n");
-        put(NodeType.BlockItem, "<BlockItem>\n");
-        put(NodeType.Block, "<Block>\n");
-        put(NodeType.BType, "<BType>\n");
-        put(NodeType.Character, "<Character>\n");
-        put(NodeType.CompUnit, "<CompUnit>\n");
-        put(NodeType.Cond, "<Cond>\n");
-        put(NodeType.ConstDecl, "<ConstDecl>\n");
-        put(NodeType.ConstDef, "<ConstDef>\n");
-        put(NodeType.ConstExp, "<ConstExp>\n");
-        put(NodeType.ConstInitVal, "<ConstInitVal>\n");
-        put(NodeType.Decl, "<Decl>\n");
-        put(NodeType.EqExp, "<EqExp>\n");
-        put(NodeType.Exp, "<Exp>\n");
-        put(NodeType.ForStmt, "<ForStmt>\n");
-        put(NodeType.FuncDef, "<FuncDef>\n");
-        put(NodeType.FuncFParam, "<FuncFParam>\n");
-        put(NodeType.FuncFParams, "<FuncFParams>\n");
-        put(NodeType.FuncRParams, "<FuncRParams>\n");
-        put(NodeType.FuncType, "<FuncType>\n");
-        put(NodeType.InitVal, "<InitVal>\n");
-        put(NodeType.LAndExp, "<LAndExp>\n");
-        put(NodeType.LOrExp, "<LOrExp>\n");
-        put(NodeType.LVal, "<LVal>\n");
-        put(NodeType.MainFuncDef, "<MainFuncDef>\n");
-        put(NodeType.MulExp, "<MulExp>\n");
-        put(NodeType.Number, "<Number>\n");
-        put(NodeType.PrimaryExp, "<PrimaryExp>\n");
-        put(NodeType.RelExp, "<RelExp>\n");
-        put(NodeType.Stmt, "<Stmt>\n");
-        put(NodeType.UnaryExp, "<UnaryExp>\n");
-        put(NodeType.UnaryOp, "<UnaryOp>\n");
-        put(NodeType.VarDecl, "<VarDecl>\n");
-        put(NodeType.VarDef, "<VarDef>\n");
-    }};
 
     private List<Token> tokens;
     private List<Error> errors;
